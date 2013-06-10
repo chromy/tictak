3d1a570dff4d3486a55a3b8e9f6fa16f99d864a1
Hector Dearman <hector.dearman@gmail.com>
2013-06-10 23:43:48 +0100

    added the first line as a hint

--- myprogram.js
+++ myprogram.js
@@ -1,22 +1,32 @@
 var stage = new Kinetic.Stage({
     container: 'container',
-    width: 500,
-    height: 500
+    width: 600,
+    height: 600
 });
 
 var layer = new Kinetic.Layer();
 
 var border = new Kinetic.Rect({
-    x: 4,
-    y: 4,
-    width: 500-8,
-    height: 500-8,
+    x: 2,
+    y: 2,
+    width: 600-6,
+    height: 600-6,
     stroke: 'black',
     strokeWidth: 4
 });
 
-// add the shape to the layer
+// The first line of our board, can you add the rest?
+var line_one = new Kinetic.Line({
+      points: [[200, 0], [200, 600-2]],
+      stroke: 'black',
+      strokeWidth: 4
+});
+
+// add the border to the layer
 layer.add(border);
+
+// add the first line
+layer.add(line_one);
 
 // add the layer to the stage
 stage.add(layer);
